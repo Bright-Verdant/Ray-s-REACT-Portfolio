aee53c7de36ce0e5968be44a915f8a5760da7176
raymond-stack-vanasse <raymond.stackvanasse@gmail.com>
2023-05-04 22:20:37 -0500

    basic portfolio funtionality

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        This Page is about ME
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/Folio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Folio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <p>
+        This is a Portfolio page
+      </p>
+    </div>
+  );
+}
